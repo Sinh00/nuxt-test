aca4642d35bb9c7a19e90ddd5312362a8d9b7ba1
Sinh00 <msoirnthe@gmail.com>
2021-02-07 03:02:02 +0900

    [add] #4 非同期でIPを取得するページの作成

---
+++ pages/axios.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <button @click="getIp">IPアドレスを取得</button>
+    <p>{{ ip }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: function () {
+    return {
+      ip: '0.0.0.0',
+    }
+  },
+  methods: {
+    async getIp() {
+      const ip = await this.$axios.$get('http://icanhazip.com')
+      this.ip = ip
+    },
+  },
+})
+</script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
